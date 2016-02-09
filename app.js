03f36dac3f40ab8a32e4b887b71f074e0542a66d
Misty Nguyen <mistymadonna@gmail.com>
2016-02-09 11:08:46 -0800

    add index and app.js to repo

---
+++ app.js
@@ -0,0 +1,16 @@
+// JS file for Feb 8 class demo
+var userName = prompt('what is your name?');
+
+console.log('the user said their name is ' + userName);
+console.log(userGame);
+
+alert('meow meow ' + userName + '!!!!!!');
+
+var userGame = prompt('what is your game?');
+
+alert ('so, ' + userName + ', I heard you like ' + userGame)
+
+var loveHate = confirm(userName + ', do you love me?');
+
+alert ('Does ' + userName + ' love me? ' + loveHate);
+console.log(userGame);
